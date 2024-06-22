d8b4f4e8cd1ea5a7797e6ada289fd86b0e56e719
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-06-23 00:17:34 +0530

    Some animation added

--- gsapwithreact/src/App.jsx
+++ gsapwithreact/src/App.jsx
@@ -1,7 +1,28 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
 const App = () => {
+  const boxRef = useRef();
+
+  useGSAP(() => {
+    gsap.from(boxRef.current, {
+      duration: 1,
+      delay: 1,
+      rotate: 360,
+    });
+  });
+
   return (
     <main>
-      <div className="box"></div>
+      <div className="container">
+        <div className="circle"></div>
+        <div ref={boxRef} className="box"></div>
+      </div>
+      <div className="kuch">
+        <div className="circle"></div>
+        <div className="box"></div>
+      </div>
     </main>
   );
 };
