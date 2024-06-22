adbb5c310d305c5a725054466eb5c17121bbef33
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-06-22 11:52:37 +0530

    scroll trigger added

---
+++ 04/script.js
@@ -0,0 +1,20 @@
+gsap.from("#page1 #box", {
+  scale: 0,
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+});
+gsap.from("#page2 #box", {
+  scale: 0,
+  duration: 2,
+  rotate: 360,
+  scrollTrigger: {
+    trigger: "#page2 #box",
+    scroller: "body",
+    markers: true,
+    start: "top 60%",
+    end: "top 30%",
+    scrub: 2,
+    pin: true,
+  },
+});
